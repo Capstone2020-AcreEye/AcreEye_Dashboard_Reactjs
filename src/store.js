aa8e9a2af14553aaf50cf2d2f5e025713b01edc5
Akif Manzoor <akifmanzoor124@gmail.com>
2020-11-23 15:40:46 -0500

    fixed merge conflict in header.js and store.js

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 import cardsReducer from './reducers/cardsReducer'
 import tasksReducer from './reducers/tasksReducer'
+import userReducer from './reducers/userReducer'
 
 const reducer = combineReducers({
     cards: cardsReducer,

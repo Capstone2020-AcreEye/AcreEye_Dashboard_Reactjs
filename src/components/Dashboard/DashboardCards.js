2cecdbf464e364db01a81300523fc5a1f2729b4e
Akif Manzoor <akifmanzoor124@gmail.com>
2020-11-25 16:00:55 -0500

    created folders for components for readability

--- src/components/Dashboard/DashboardCards.js
+++ src/components/Dashboard/DashboardCards.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {initializeCards} from '../reducers/cardsReducer'
+import {initializeCards} from '../../reducers/cardsReducer'
 
 const DashboardCards = () => {
     const dispatch = useDispatch()

004938a820d410115784f26cf173c02faca80271
Adil <aughr063@uottawa.ca>
2020-11-25 21:43:38 -0500

    Implemented linegraph functionality
    Temperature data is fetched from database and is displayed on linegraph
    using the canvasjs library

---
+++ src/components/LineGraph.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CanvasJSReact from '../assets/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+function LineGraph({options}) {
+
+    return (
+        <div>
+            <CanvasJSChart options = {options} />
+        </div>
+    )
+}
+
+export default LineGraph

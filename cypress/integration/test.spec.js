20a79b6057c934011bdd9e1eef5386e1ee7d5646
Akif Manzoor <akifmanzoor124@gmail.com>
2021-01-28 21:43:35 -0500

    added new test for login

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,5 +1,6 @@
 describe('My First Test', () => {
     it('Does not do much!', () => {
         cy.visit('http://www.localhost:3000/')
+        cy.contains('form-control form-control-user')
     })
 })

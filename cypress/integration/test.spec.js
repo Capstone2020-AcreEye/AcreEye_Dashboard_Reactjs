3c2291159bf9867f9d0803b2e557ab691f1717e4
Akif Manzoor <akifmanzoor124@gmail.com>
2021-01-29 15:03:34 -0500

    changed to localhost

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,7 @@
 describe('Login', () => {
     it('Checks for login input', () => {
         console.log()
-        cy.visit('http://127.0.0.1:3000')
+        cy.visit('http://localhost:3000')
         cy.get('#exampleInputEmail')
     })
 })

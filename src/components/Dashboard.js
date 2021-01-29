b67315749306e8b44fa525febc8154292f54400d
Akif Manzoor <akifmanzoor124@gmail.com>
2021-01-28 19:09:28 -0500

    added test folder with selenium testing in end-to-end folder, other minor fixes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,7 +10,7 @@ import HomeDashboard from './Dashboard/HomeDashboard';
 import Footer from './Footer'
 import Analytics from './Analytics/Analytics'
 import Settings from './Settings'
-import AddTaskModal from './Dashboard/AddTaskModal'
+// import Profile from './Profile'
 
 const Dashboard = () => {
     return (
@@ -26,8 +26,8 @@ const Dashboard = () => {
                         <Header></Header>
                         <div className="container-fluid">
                             <Switch>
-                                <Route path='/AddTaskModal'>
-                                    <AddTaskModal/>
+                                <Route path='/profile'>
+                                    <Settings />
                                 </Route>
                                 <Route path="/analytics">
                                     <Analytics />

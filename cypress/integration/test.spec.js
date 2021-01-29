af96ca64cabf001cf9c313405381bf209ddc2aee
Akif Manzoor <akifmanzoor124@gmail.com>
2021-01-29 10:01:51 -0500

    tries to run the server locally

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,6 +1,6 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
+describe('Login', () => {
+    it('Checks for login input', () => {
         cy.visit('http://www.localhost:3000/')
-        cy.contains('form-control form-control-user')
+        cy.contains('.form-control form-control-user')
     })
 })
